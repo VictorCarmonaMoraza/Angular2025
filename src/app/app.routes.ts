d9423c296b1782884de877766ff427ea8e0088b7
Victor Carmona <victorcarmona9035.com>
2025-09-04 22:58:24 +0200

    Creando nuestro primer componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ViewMain } from './pages/view/view-main/view-main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ViewMain }
+];
---
+++ src/app/pages/view/view-main/view-main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-main',
+  imports: [],
+  templateUrl: './view-main.html',
+  styleUrl: './view-main.css'
+})
+export class ViewMain {
+
+}
